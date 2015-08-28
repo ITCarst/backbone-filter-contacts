df5deeeb07ad2e9498d632ea5c01b02894c1f343
IonutC <icarst@carst.me>
2015-08-28 11:10:59 +0200

    added comments in the php file

--- contacts.php
+++ contacts.php
@@ -6,8 +6,12 @@ ini_set('display_startup_errors',1);
 
 class contactManager 
 {
+    //create private var to keep mysqli db info
     private $dbResponse;
 
+    /*
+    * Main Constructor 
+    */
     public function __construct () 
     {
         //db connection
@@ -18,8 +22,15 @@ class contactManager
            $this->dbData();
     }
 
-   private function connect () 
-   {
+    /*
+     * Connect to database using user and pass also name the databse
+     * Assigns the databse to the private $dbResponse
+     * @param {$u} - db username
+     * @param {$p} - db password
+    */
+    private function connect ($u, $p) 
+    {
+        //create database connection and select the need db
        $data = mysqli_connect('localhost:8889', 'root', 'root', "backbone_contacts");
        
        if ($data->connect_errno) {
@@ -30,6 +41,9 @@ class contactManager
        $this->dbResponse = $data;
     }
 
+    /*
+     * Select data from the database based on the request method
+    */
     private function dbData () 
     {
         switch($_SERVER["REQUEST_METHOD"]) 
@@ -43,23 +57,27 @@ class contactManager
             case "POST":
                 print_r($_POST);
                 break;
-
             case "DELETE":
                 print_r("delete");
                 break;
-
             case "PUT":
                 print_r("insert");
                 break;
-        } 
+        }
+        //close the connction 
         mysqli_close($this->dbResponse);
     }
-
+    /*
+     * Select the contacts from the database 
+     * @param {$select} - string, that includes the asked columns from the table
+     * @param {type} - WHERE clause that selects the contact based on 'type' filed
+    */
     private function selectFromDb ($select,  $type) 
     {
         $db = $this->dbResponse;
         $resultArray = [];
 
+        //for the $type all grab all the entries
         if ($type == "all")
             $selectAll = "SELECT " . $select. " FROM contacts ";
         else 
@@ -68,18 +86,21 @@ class contactManager
         //get all the entries from db
         if ($result = mysqli_query($db, $selectAll)) 
         {   
+            //loop through the result using assoc
             while ($row = $result->fetch_assoc()) 
             {   
+                //add the entries into the $resultArray
                 if (!empty($row)) 
                     $resultArray[] = $row;
             }   
+            //return the array with the entries
             return $resultArray;
-
         } else {
+            //return a plain string so that we have something to display if there is no data 
             return "No results found";
         }   
     }
 }
 
-
+//init the ContactManager class
 $contactManger = new ContactManager();
