fd64a4fa6486bcf0e4ff59d0c52563e9f5f0389c
IonutC <icarst@carst.me>
2015-09-06 12:16:42 +0200

    moved files in different folders

--- js/app/main.js
+++ js/app/main.js
@@ -1,9 +1,9 @@
 require.config({
     baseUrl: "./js/app",
     paths: {
-        jquery: "../node_modules/jquery/dist/jquery.min",
-        underscore: "../node_modules/underscore/underscore-min",
-        backbone: "../node_modules/backbone/backbone-min",
+        jquery: "../../node_modules/jquery/dist/jquery.min",
+        underscore: "../../node_modules/underscore/underscore-min",
+        backbone: "../../node_modules/backbone/backbone-min",
         templates: "templates",
         text: "../libs/text"
     }
